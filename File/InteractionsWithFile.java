4db79430aa492ac59f94e96047d8d294e49e848d
Архипов Максим <solomon7dun@gmail.com>
2018-04-14 17:23:52 +0300

    added section for interact with text files

---
+++ File/InteractionsWithFile.java
@@ -0,0 +1,33 @@
+package third_week.PatternRecognition;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InteractionsWithFile {
+    // read all information from file and return String which contains that information
+    public static String readFileToString (String fileName) {
+        String readedFromFile = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            StringBuilder sb = new StringBuilder();
+            do {
+                line = br.readLine();
+                if(line == null)
+                    break;
+                sb.append(line);
+            } while(true);
+
+            readedFromFile = sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return readedFromFile;
+    }
+}
